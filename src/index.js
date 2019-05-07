28cb3763214ebe92bac97503da8625c99de1703d
ChristopherZev <christopher_zev@icloud.com>
2019-05-07 18:10:30 -0400

    Added Semantic UI to index.html via cloudflare for blog post styling, Added/imported faker.js to generate fake data for use in this project, generated a random avatar image via interpolation {faker.image.avatar()}, cloned the blog post 3 times for a simplistic way to generate 3 blog posts on screen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//imported Faker.js to generate fake data to use in projects
+import faker from 'faker';
+
 
 const App = () => {
-	return <div>Hello There!</div>
+	return (
+		// "ui container comments", "comment", "avatar", "metadata"
+		//	"date", "content", "author", "text" are all classes from 
+		//	semantic UI
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					{/* {faker.image.avatar()} is called via interpolation
+					to generate a random image for this tag from the
+					faker import.*/}
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date"> Today at 6:00PM</span>
+					</div>
+
+					<div className="text"> Nice Blog Post!</div>
+				</div>
+			</div>
+
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date"> Today at 6:00PM</span>
+					</div>
+
+					<div className="text"> Nice Blog Post!</div>
+				</div>
+			</div>
+
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date"> Today at 6:00PM</span>
+					</div>
+
+					<div className="text"> Nice Blog Post!</div>
+				</div>
+			</div>
+
+		</div>
+
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
